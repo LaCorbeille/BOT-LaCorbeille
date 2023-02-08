88a618393b04ab73d25b00599f242b464a93cac2
Noa Second <38381564+YoruKiwi@users.noreply.github.com>
2023-02-08 21:01:52 +0100

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,14 +1,8 @@
 const Discord = require("discord.js");
 const { ActionRowBuilder, ButtonBuilder, ButtonStyle, Events } = require('discord.js');
 
-// BADWORDS
-const badwords = [
-    "Ta mère la jonquille.",
-    "J'te pied bouche.",
-    "Fils de chauve.",
-    "Fils de Jul.",
-    "Tg.",
-    "Apprend à te taire.",
+// GIF
+const gifList = [
     "https://tenor.com/view/awkward-smile-wave-hi-hello-gif-18570699",
     "https://tenor.com/view/rick-and-morty-mr-poopy-butt-hole-ooooh-weeee-gif-9826117",
     "https://tenor.com/view/nounours-bonne-nuit-bonne-nuit-les-petits-bonne-nuit-les-tipeus-mange-tes-morts-gif-17991623",
@@ -17,11 +11,7 @@ const badwords = [
     "https://tenor.com/view/steve-harvey-smh-shake-my-head-shocked-surprised-gif-5057255"];
 
 module.exports = async (bot, message) => {
-    // LGBT FLAG
-    if(message.content.toLowerCase().includes('lgbt')||message.content.toLowerCase().includes('gay')) {
-        message.reply(':rainbow:');
-    } 
-    // RANDOM BADWORD or RICKROLL
+    // RANDOM GIF or RICKROLL
     if (Math.random() < 0.1) {
         if(Math.random() < 0.1) {
             // RICKROLL
@@ -34,9 +24,9 @@ module.exports = async (bot, message) => {
             );    
             await message.reply({ content: 'Je pense que tu ne devrais pas toucher', components: [row] });
         } else {
-            // BADWORD
-            let randomIndex = Math.floor(Math.random() * badwords.length);
-            message.reply(badwords[randomIndex]);
+            // GIF
+            let randomIndex = Math.floor(Math.random() * gifList.length);
+            message.reply(gifList[randomIndex]);
         }
     } else {
         if (message.content.toLowerCase().endsWith('quoi')) {
@@ -45,14 +35,6 @@ module.exports = async (bot, message) => {
             } else {
                 message.reply('feur.');
             }
-        } else if (message.content.toLowerCase().endsWith('rouge')) {
-            message.reply('gorge');
-            message.reply('profonde');
-            message.reply(':eggplant: :sweat_drops:');
-        } else if (message.content.toLowerCase().endsWith('oui')) {
-            message.reply('fi');   
-        } else if (message.content.toLowerCase().endsWith('non')) {
-            message.reply('bril');   
-        }
+        } 
     }
 }
