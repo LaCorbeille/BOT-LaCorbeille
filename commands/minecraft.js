6fcffcaaa3918b8dd28592463f376ce7cf7f8485
sn103354 <noa.second@etu.unice.fr>
2023-02-13 21:49:32 +0100

    Fix minecraft.js
    Fix display offline status

--- commands/minecraft.js
+++ commands/minecraft.js
@@ -15,26 +15,40 @@ module.exports = {
         try {
             let res = await axios.get(`https://mcapi.us/server/status?ip=${ip}`);
             let data = res.data;
+            let Embed = new Discord.EmbedBuilder();
                         
-            let Embed = new Discord.EmbedBuilder()
-            .setColor(bot.color)
-            .setTitle("Serveur Minecraft La Corbeille")
-            .setThumbnail(bot.user.displayAvatarURL({dynamic: true}))
-            .setDescription(`Status :`)
-            .addFields(
-                { name: "IP du serveur", value: ip, inline: true },
-                { name: "IP accessible depuis l'IUT", value: ip+":80", inline: true },
-                { name: "Version du serveur", value: data.server.name, inline: false },
-                { name: "Statut du serveur", value: data.online ? "En ligne :green_circle:" : "Hors ligne :red_circle:", inline: true },
-                { name: "Joueurs en ligne", value: `${data.players.now} / ${data.players.max}`, inline: true }
-            )
-            .setTimestamp()
-            .setFooter({text: "API : mcapi.us"});
+            if (data.online) {
+                Embed.setColor(bot.color)
+                .setTitle("Serveur Minecraft La Corbeille")
+                .setThumbnail(bot.user.displayAvatarURL({dynamic: true}))
+                .setDescription(`Status :`)
+                .addFields(
+                    { name: "IP du serveur", value: ip, inline: true },
+                    { name: "IP accessible depuis l'IUT", value: ip+":80", inline: true },
+                    { name: "Version du serveur", value: data.server.name, inline: false },
+                    { name: "Statut du serveur", value: data.online ? "En ligne :green_circle:" : "Hors ligne :red_circle:", inline: true },
+                    { name: "Joueurs en ligne", value: `${data.players.now} / ${data.players.max}`, inline: true }
+                )
+                .setTimestamp()
+                .setFooter({text: "API : mcapi.us"});
+            } else {
+                Embed.setColor(bot.color)
+                .setTitle("Serveur Minecraft La Corbeille")
+                .setThumbnail(bot.user.displayAvatarURL({dynamic: true}))
+                .setDescription(`Status :`)
+                .addFields(
+                    { name: "IP du serveur", value: ip, inline: true },
+                    { name: "IP accessible depuis l'IUT", value: ip+":80", inline: true },
+                    { name: "Statut du serveur", value: data.online ? "En ligne :green_circle:" : "Hors ligne :red_circle:", inline: false }
+                )
+                .setTimestamp()
+                .setFooter({text: "API : mcapi.us"});
+            }            
             
             await message.reply({embeds: [Embed]})
         } catch (error) {
             console.error(error);
-            message.reply("Une erreur est survenue lors de la récupération des informations du serveur.");
+            message.reply("Le serveur semble être éteint.");
         }
     }
 }
