7badc599d533bab3e3b62d126f5efbc64bd640a7
Noa Second <contact@noasecond.com>
2024-07-07 19:19:02 +0200

    :sparkles: Add alert when bot is running

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const fs = require("fs");
 const config = require("../config.json");
 const loadSlashCommand = require("../loaders/loadSlashCommands");
 
@@ -20,5 +21,19 @@ module.exports = async bot => {
     } catch (error) {
         //console.error(error);
         console.log(`[LaCorbeille - ready] Impossible de changer le nom.`);
-    }   
+    }
+
+    // Alert users when bot is ready
+    const guild = bot.guilds.cache.get("411196282124894208");
+    const version = fs.readFileSync("./VERSION", "utf-8").trim();
+    if (guild) {
+        const channel = guild.channels.cache.get("411196282124894210");
+        if (channel && channel.isTextBased()) {
+            channel.send(`Le bot a été mis à jour vers la version ${version} !`);
+        } else {
+            console.log(`[LaCorbeille - ready] Impossible de trouver le salon.`);
+        }
+    } else {
+        console.log(`[LaCorbeille - ready] Impossible de trouver le serveur.`);
+    }
 }
